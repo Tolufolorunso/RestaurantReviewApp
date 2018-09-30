e129120e89e8e156716d91102aa3028b2180d3a0
Folorunso Tolulope Kola <tolufolorunso@yahoo.com>
2018-09-29 19:46:03 -0700

    changes made

--- mrWorker.js
+++ mrWorker.js
@@ -6,7 +6,6 @@ const cachesFiles = [
     '/',
     '/index.html',
     '/restaurant.html',
-    '/404.html',
     '/css/styles.css',
     '/js/dbhelper.js',
     '/js/main.js',
@@ -27,13 +26,11 @@ const cachesFiles = [
 //install EventListener
 
 self.addEventListener('install', (e) => {
-    console.log('installed');
 
     e.waitUntil(
         caches
         .open(swName)
         .then((cache) => {
-            console.log('working');
             return cache.addAll(cachesFiles);
         })
         .then(() => self.skipWaiting())
@@ -46,14 +43,12 @@ self.addEventListener('install', (e) => {
 //activate EventListener
 
 self.addEventListener('activate', (e) => {
-    console.log('activate');
     //removing unwanted cache
     e.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cacheName) => {
                     if (cacheName !== swName) {
-                        console.log('good');
                         return caches.delete(cacheName);
                     }
                 })
@@ -68,15 +63,13 @@ self.addEventListener('activate', (e) => {
 //fetch EventListener
 
 self.addEventListener('fetch', (e) => {
-    console.log('fetching');
+    //    console.log('fetching');
     e.respondWith(
         caches.match(e.request)
         .then((response) => {
             if (response) {
-                console.log(e.request);
                 return response;
             } else {
-                console.log(e.request);
                 return fetch(e.request)
                     .then((response) => {
                         //clone the response
@@ -88,7 +81,7 @@ self.addEventListener('fetch', (e) => {
                             })
                         return response;
                     })
-                    .catch((err) => caches.match('404.html'));
+                    .catch((err) => console.error(err));
             }
 
         })
